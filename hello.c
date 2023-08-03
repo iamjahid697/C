a8b8dd851bd2a3bb9ab72d5e1b0334e941f3040d
iamjahid697 <iamjahid697@gmail.com>
2023-08-03 21:27:44 +0600

    first commit

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Hello world\n");
+    scanf("%d", &number);
+    printf("Number is :- %d\n", number);
+    return 0;
+}
---
+++ square.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int side;
+    printf("Enter side ");
+    scanf("%d", &side);
+    printf("Side is %d\n", side * side);
+    return 0;
+}
