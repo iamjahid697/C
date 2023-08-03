df200c11aef5d11997757587a9cca5df82fc4efa
iamjahid697 <iamjahid697@gmail.com>
2023-08-03 21:39:17 +0600

    square using float

--- square.c
+++ square.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+// area of square in integer
 int main()
 {
     int side;
     printf("Enter side ");
     scanf("%d", &side);
     printf("Side is %d\n", side * side);
+    return 0;
+}
+
+// area of square in float
+int main()
+{
+    float side;
+    printf("Enter side");
+    scanf("%f", &side);
+    printf("area is : %f\n", side * side);
     return 0;
 }
